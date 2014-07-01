1c4be151642c905e40862fef2468ec25bc29d49e
ljakobovits <ljakobovits@tcc-ljackobovits2.local>
2014-07-01 14:21:11 -0700

    add cool pose

--- js/app.js
+++ js/app.js
@@ -1,19 +1,24 @@
 $(document).ready(function() {
 
+
 function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
   $('#hadouken-sound')[0].play();
 }
 
+
 	$('.ryu').mouseenter(function() {
     	$('.ryu-still').hide();
 		$('.ryu-ready').show();
   	})
+
+
  .mouseleave(function() {
  	$('.ryu-still').show();
- 	$('ryu-ready').hide();
+ 	$('.ryu-ready').hide();
  	})
+ 
   .mousedown(function() {
     playHadouken();    
     $('.ryu-ready').hide();
@@ -28,10 +33,30 @@ function playHadouken () {
   		}
 	); 
 })
+  
   .mouseup(function() {
   	$('.ryu-throwing').hide();
   	$('.ryu-ready').show();
-  });
+  })
+
+
+
+  $('body').keydown(function(e) {
+   if (e.which == 88) {
+  	$('.ryu-ready').hide();
+  	$('.ryu-cool').show();
+  }
+
 })
+
+.keyup(function() {
+  	$('.ryu-cool').hide();
+  	$('.ryu-still').hide();
+  	$('.ryu-ready').show();
+  	$('.ryu-throwing').hide();
+ }); 
+
+
+});
 
 /* how to make hover JUST over image? */
